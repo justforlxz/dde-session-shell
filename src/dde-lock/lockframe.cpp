458817463338fd97d7aa6fe5534d8e66187726f0
xuyanghe <xuyanghe@uniontech.com>
2020-05-19 18:17:22 +0800

    Fix(lock):return to table if unclock quickly
    Task#21190
    概率出现锁屏后框内无提示语且自动进桌面后又退回锁屏界面.上一个提交改的有问题

--- src/dde-lock/lockframe.cpp
+++ src/dde-lock/lockframe.cpp
@@ -55,9 +55,7 @@ LockFrame::LockFrame(SessionBaseModel *const model, QWidget *parent)
         hide();
     });
     connect(model, &SessionBaseModel::authFinished, this, [ = ](bool success){
-        if (success) {
-            m_content->beforeUnlockAction(success);
-        }
+        m_content->beforeUnlockAction(success);
     });
 }
 
